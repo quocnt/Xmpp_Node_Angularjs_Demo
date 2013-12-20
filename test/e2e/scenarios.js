angular/angular-phonecat

step-7 $route and app partitioning
- Introduce the [$route] service which allows binding URLs for deep-linking with
  views
  - Create PhoneCatCtrl which governs the entire app and contains $route
    configuration
  - Map `/phones' to PhoneListCtrl and partails/phones-list.html
  - Map `/phones/<phone-id>' to PhoneDetailCtrl and partails/phones-detail.html
  - Copy deep linking parameters to root controller `params` property for access
    in sub controllers
  - Replace content of index.html with [ng:view] widget
- Create phone list route
  - Preserve existing PhoneListCtrl controller
  - Move existing html from index.html to partials/phone-list.html
- Create phone details route
  - Empty placeholder PhoneDetailsCtrl controller
  - Empty placeholder partials/phane-details.html template

test/e2e/scenarios.js
@@ -1,10 +1,16 @@
 /* jasmine-like end2end tests go here */
 describe('PhoneCat App', function() {
 
+  it('should redirect index.html to index.html#/phones', function() {
+    browser().navigateTo('../../app/index.html');
+    expect(browser().location().hash()).toBe('/phones');
+  });
+
+
   describe('Phone list view', function() {
 
     beforeEach(function() {
-      browser().navigateTo('../../app/index.html');
+      browser().navigateTo('../../app/index.html#/phones');
     });
 
 
@@ -38,6 +44,19 @@ describe('PhoneCat App', function() {
       input('query').enter('nexus');
       element('.phones li a').click();
       expect(browser().location().hash()).toBe('/phones/nexus-s');
+    });
+  });
+
+
+  describe('Phone detail view', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('../../app/index.html#/phones/nexus-s');
+    });
+
+
+    it('should display placeholder page with phoneId', function() {
+      expect(binding('params.phoneId')).toBe('nexus-s');
     });
   });
 });

quocnt/Xmpp_Node_Angularjs_Demo

  - Load the ngRoute module
  - Replace content of index.html with [ngView] directive

test/e2e/scenarios.js
@@ -4,10 +4,16 @@
 
 describe('PhoneCat App', function() {
 
+  it('should redirect index.html to index.html#/phones', function() {
+    browser().navigateTo('app/index.html');
+    expect(browser().location().url()).toBe('/phones');
+  });
+
+
   describe('Phone list view', function() {
 
     beforeEach(function() {
-      browser().navigateTo('app/index.html');
+      browser().navigateTo('app/index.html#/phones');
     });
 
 
@@ -41,6 +47,19 @@ describe('PhoneCat App', function() {
       input('query').enter('nexus');
       element('.phones li a').click();
       expect(browser().location().url()).toBe('/phones/nexus-s');
+    });
+  });
+
+
+  describe('Phone detail view', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('app/index.html#/phones/nexus-s');
+    });
+
+
+    it('should display placeholder page with phoneId', function() {
+      expect(binding('phoneId')).toBe('nexus-s');
     });
   });
 });